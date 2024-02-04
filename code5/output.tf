3c6de8da5fb5c2b250d07ec81c0f740a7763b317
Farelle Lagne <lagnefarelle@gmail.com>
2024-02-03 22:34:17 -0700

    terraform code

---
+++ code5/output.tf
@@ -0,0 +1,11 @@
+output "instance_ip" {
+  value = aws_lightsail_instance.custom.public_ip_address
+}
+output "my-arn" {
+ value =aws_lightsail_instance.custom.arn  
+}
+output "private" {
+ value =aws_lightsail_instance.custom.private_ip_address  
+    
+  
+}
